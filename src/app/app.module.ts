4729113a34910db319631a5aa9716a6cb8f32212
Guilherme Cabral <guicabralprowork@gmail.com>
2023-07-18 16:37:58 -0300

    Refactor: remoção do component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { MenuComponent } from './header/menu/menu.component';
 
 //Material Designer
 
 import { MatMenuModule } from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MenuComponent,
   ],
   imports: [
     BrowserModule,
